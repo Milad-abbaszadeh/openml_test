34f62e351fcb7c533119f7a1f95efe31cd418a25
milad-abbaszadeh <milad.abbaszadeh94@gmail.com>
2020-03-15 06:50:28 +0100

    14 march

--- point_builder_openmlspace_step2.py
+++ point_builder_openmlspace_step2.py
@@ -245,7 +245,7 @@ def point_builder(what_we_have,space):
 import pickle
 points_list_runs_component_32 = pickle.load(open("/home/dfki/Desktop/Thesis/openml_test/pickel_files/3/list_runs_component_3_all_flow.p", "rb"))
 print(len(points_list_runs_component_32))
-runner = Run_hyperopt(31,31)
+runner = Run_hyperopt(3,3)
 search_space = runner.make_search_space()
 points_ready_turn_totrials=[]
 for point in points_list_runs_component_32:
@@ -257,7 +257,7 @@ for point in points_list_runs_component_32:
             points_ready_turn_totrials.append(new_point)
     except:
         print("Except")
-pickle.dump(points_ready_turn_totrials, open('/home/dfki/Desktop/Thesis/openml_test/pickel_files/3/points_ready_turn_totrials_3.p','wb'))
+# pickle.dump(points_ready_turn_totrials, open('/home/dfki/Desktop/Thesis/openml_test/pickel_files/3/points_ready_turn_totrials_3.p','wb'))
 
 print(len(points_ready_turn_totrials))
 
